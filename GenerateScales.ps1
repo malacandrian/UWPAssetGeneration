18bca2e091ca682cca56379969a6dd9f7ad6623b
Evan Williams <evan@natcs.eu>
2016-04-18 23:05:37 +0100

    Changed how the script determines sizes
    Microsoft's requirements for scales are not consistant enough to simply apply the same rules to every file. Additionally, the script's math & Microsoft's math didn't always line up precisely on the 125 & 150 scales.
    
    As such all the scaling information is now included as metadata in the template files.

--- GenerateScales.ps1
+++ GenerateScales.ps1
@@ -12,6 +12,10 @@
 ##  See the License for the specific language governing permissions and
 ##  limitations under the License.
 
+########################
+# Set Script Variables #
+########################
+
 #Set the location of the "inkscape.com" program 
 $inkscape = "C:\Program Files\Inkscape\Inkscape.com"
 
@@ -25,28 +29,72 @@ $inputFolder = "Templates\"
 #Set the output directory for the scaled images (relative path from script location)
 $outputFolder =".\"
 
-#############################
-#Do not edit below this line#
-#############################
+#######################
+#End Script Variables #
+#######################
+
+#Fetch the resize module
 Import-Module .\Resize-Image.psm1
 
-$scales = $scales | Sort-Object -Descending
-$largestScale = $scales[0]
+#Function to use inkscape to export an SVG as a png
+function Export-SVG {
+    param( [String] $inFile, [String] $outfile)
+    . $inkscape -f $inFile -e $outfile
+}
 
+#Function to generate a filename in the format required by the UWP
+function Generate-Filename {
+    param( [string] $coreName, [string] $suffix, [int] $scale)
+    
+    Write-Output "$coreName.$suffix-$scale.png"
+}
+
+#Work out where this script is stored, so relative paths will work
 $fullLocation = $MyInvocation.MyCommand.Source
 $location = $FullLocation.Substring(0,$fullLocation.LastIndexOf($MyInvocation.MyCommand.Name))
 
+#Grab all the non-template svgs in the input foder
 Get-ChildItem $inputFolder | where-object { $_.extension -eq ".svg" -and $_.BaseName -notlike "*_Template" } | ForEach-Object {
-    $fullname = $_.FullName
-    $name = $_.BaseName
+    
+    #Get the resize data from the file
+    #If the file does not contain resize data, move on to the next file.
+    [xml]$file = Get-Content $inputFolder\$_
+    if($file.svg.Sizes) {
+        #Get the sizes it needs to be scaled to
+        #Currently this requires the sizes to be in order (or at the very least, start with the largest)
+        #Would be good to sort them, but cannot work out how currently, as Sort-Object treats "Width" and "Height" as strings
+        $sizes = $file.svg.Sizes.Size
 
-    $curOutputFolder = "$outputFolder\$name"
-    if(-not (Test-Path $curOutputFolder -PathType Container)) { New-Item $curOutputFolder -ItemType Directory }
+        #Get the name of the current SVG. 
+        $fullname = $_.FullName
+        $name = $_.BaseName
 
+        #Get the output folder, create it if it doesn't yet exist
+        $curOutputFolder = "$outputFolder\$name"
+        if(-not (Test-Path $curOutputFolder -PathType Container)) { New-Item $curOutputFolder -ItemType Directory }
+        
+        #Export the first png, from which all the others will be scaled
+        $firstScale = $sizes[0].Scale
+        $firstFile = Generate-Filename -coreName $_.BaseName -suffix $sizes[0].NameSuffix -scale $firstScale
+        $firstFilePath = "$curOutputFolder\$firstFile"
+        Export-SVG -inFile $_.FullName -outfile $firstFilePath
 
-    $newName = "$curOutputFolder\$name.Scale-$largestScale.png"
+        #Resize the png to all the required sizes
+        $sizes | ForEach-Object {
+            #Make sure we're not trying to scale the first image to itself
+            if($_.Scale -ne $firstScale) {
+                #Get the full path for where the file needs to output
+                $newName = Generate-Filename -coreName $name -suffix $_.NameSuffix -scale $_.Scale
+                $fullOutputPath = "$location\$curOutputFolder\$newName"
+                Write-Output $fullOutputPath
 
-    . $inkscape -f $fullname -e $newName
+                #Resize the image
+                Resize-Image -InputFile $firstFilePath -OutputFile $fullOutputPath -Width $_.Width -Height $_.Height
+            }
+        }
+    }
+
+    <#
 
     $scales | ForEach-Object {
         $scaleFactor = $_ / $largestScale
@@ -55,4 +103,5 @@ Get-ChildItem $inputFolder | where-object { $_.extension -eq ".svg" -and $_.Base
             Resize-Image -InputFile ".\$newName" -OutputFile "$location\$curOutputFolder\$name.Scale-$_.png" -Scale ($scaleFactor * 100)
         }
     }
+    #>
 }
